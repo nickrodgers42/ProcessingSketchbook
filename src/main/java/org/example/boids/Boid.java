be0bc867c5bf58281f1ee9a1c9a55fe73473691c
Nick Rodgers <nick.rodgers42@gmail.com>
2022-10-23 01:31:43 -0400

    Update boid class

--- src/main/java/org/example/boids/Boid.java
+++ src/main/java/org/example/boids/Boid.java
@@ -1,5 +1,7 @@
 package org.example.boids;
 
+import processing.core.PApplet;
+
 public class Boid {
     private Vector2 position;
     private Vector2 velocity;
@@ -9,5 +11,42 @@ public class Boid {
         this.position = new Vector2();
         this.velocity = new Vector2();
         this.acceleration = new Vector2();
+    }
+
+    public Boid(Vector2 position, Vector2 velocity, Vector2 acceleration) {
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+    }
+
+    public void update(double deltaTime) {
+
+    }
+
+    public void drawBoid(PApplet sketch) {
+        sketch.pushMatrix();
+        sketch.translate((int)position.getX(), (int)position.getY());
+        sketch.rotate((float) velocity.heading());
+        sketch.fill(0, 255, 255);
+        sketch.triangle(
+            0, -10,
+            -5, 5,
+            5, 5);
+        sketch.popMatrix();
+    }
+
+    public void drawPerceptionRadius(PApplet sketch) {
+        sketch.pushMatrix();
+        sketch.translate((int)position.getX(), (int)position.getY());
+        sketch.arc(
+            0, 0, 100, 100,
+            (float)(3 * Math.PI / 4),
+            (float)(Math.PI / 4 + 2 * Math.PI));
+        sketch.popMatrix();
+    }
+
+    public void draw(PApplet sketch) {
+        drawPerceptionRadius(sketch);
+        drawBoid(sketch);
     }
 }
--- src/main/java/org/example/boids/Sketch.java
+++ src/main/java/org/example/boids/Sketch.java
@@ -5,14 +5,30 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
     private DeltaTime deltaTime = new DeltaTime();
 
+    private Boid testBoid;
+
     @Override
     public void settings() {
         size(500, 500);
     }
 
     @Override
-    public void draw() {
+    public void setup() {
+        testBoid = new Boid(
+            new Vector2(this.width / 2, this.height / 2),
+            new Vector2(0, 0),
+            new Vector2(0, 0));
+    }
+
+    public void update() {
         double dt = deltaTime.update();
+        testBoid.update(dt);
+    }
+
+    @Override
+    public void draw() {
+        update();
         background(255);
+        testBoid.draw(this);
     }
 }
